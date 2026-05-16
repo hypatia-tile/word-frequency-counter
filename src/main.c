9ff53cc3a8728ceef10f856adb1bf65ac16ddf7e
shinokun <hypatia.tile02021@gmail.com>
2026-05-17 02:05:35 +0900

    feat: add hash function.

--- src/main.c
+++ src/main.c
@@ -7,6 +7,15 @@ typedef struct {
   Value value;
 } Entry;
 
+uint32_t hashString(const char *key, int length) {
+  uint32_t hash = 2166136261u;
+  for (int i = 0; i < length; i++) {
+    hash ^= (uint8_t) key[i];
+    hash *= 16777619;
+  }
+  return hash;
+}
+
 int main() {
   return 0;
 }
